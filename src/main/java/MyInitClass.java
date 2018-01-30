ae6d076d6805511403a511d09b50b033b09633e5
wsobasik <ronin12@wp.pl>
2018-01-30 15:50:54 +0100

    dodanie znmiany tenanta

--- src/main/java/MyInitClass.java
+++ src/main/java/MyInitClass.java
@@ -4,12 +4,13 @@ import java.util.regex.Pattern;
 
 public class MyInitClass {
 
-
     private final String PREFIX = "_v(\\d+).xml";
     private String tmpFile = System.getProperty("user.dir") + "\\tempTmp.xml";
     private Long version;
     Pattern versionPattern = Pattern.compile("version=\"(\\d+)\"");
     Pattern fileVersionPattern = Pattern.compile(PREFIX);
+    Pattern tenantBKPattern1 = Pattern.compile("tenantBusinessKey=\"\\w+\"");
+    Pattern tenantBKPattern2 = Pattern.compile("tenant business-key=\"\\w+\"");
 
 
     public void updateVersionToANewFile(String orgInputFile) {
@@ -24,8 +25,6 @@ public class MyInitClass {
                 bufferedWriter.write(newLine);
                 bufferedWriter.newLine();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -83,5 +82,36 @@ public class MyInitClass {
 
     public void setVersion(Long version) {
         this.version = version;
+    }
+
+    public void updateVersionToANewFile(String orgInputFile, String tenantName) {
+        try (
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tmpFile));
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(orgInputFile))
+        ) {
+            String currentLine;
+            String newLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                newLine = increseVersionNumber(currentLine);
+                newLine = changeTenantName(currentLine, tenantName);
+                bufferedWriter.write(newLine);
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String changeTenantName(String currentLine, String tenantName ) {
+        Matcher matcher1 = tenantBKPattern1.matcher(currentLine);
+        Matcher matcher2 = tenantBKPattern2.matcher(currentLine);
+        if (matcher1.find()) {
+            return matcher1.replaceAll("tenantBusinessKey=\"" + tenantName + "\"");
+        } else {
+            if (matcher2.find()) {
+                return matcher2.replaceAll("tenant business-key=\"" + tenantName + "\"");
+            }
+            return currentLine;
+        }
     }
 }
--- src/main/java/StartApplication.java
+++ src/main/java/StartApplication.java
@@ -4,12 +4,19 @@ public class StartApplication {
     public static void main(String[] args) {
 
         String orgInputFile = args[0];
-     //   String orgInputFile = "C:\\workspace\\VersionUpdater\\001\\out\\artifacts\\001_jar\\temp1111_v.11.xml";
+        String tenantName = null;
         MyInitClass myInitClass = new MyInitClass();
-        myInitClass.updateVersionToANewFile(orgInputFile);
-        myInitClass.renameTheOutputFile(orgInputFile);
 
 
+        if (args.length > 1) {
+            tenantName = args[1];
+            myInitClass.updateVersionToANewFile(orgInputFile,tenantName);
+            myInitClass.renameTheOutputFile(orgInputFile);
+        } else {
+            myInitClass.updateVersionToANewFile(orgInputFile);
+            myInitClass.renameTheOutputFile(orgInputFile);
+        }
     }
-
 }
+// tenantBusinessKey="IT5"
+// tenant business-key="IT5"
